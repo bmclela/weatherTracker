1ca42b5947441d0629cd977426e23c64a29bf9c0
Ben <ben.mcleland@gmail.com>
2020-03-02 16:36:52 -0500

    commit

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ app.get('/', (req, res) => {
     'https://api.openweathermap.org/data/2.5/weather?q=Charlotte&appid=60ef1c767a56bb535edc4550cc47cd7d&units=imperial';
   https.get(url, response => {
     console.log(response.statusCode);
+
+    response.on('data', data => {
+      const weatherData = JSON.parse(data);
+      console.log(weatherData);
+      const temp = weatherData.main.temp;
+      console.log(temp);
+      const description = weatherData.weather[0].description;
+      console.log(description);
+    });
   });
   res.send('Server is up and running.');
 });
